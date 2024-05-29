8a78162224bcfa9f152d497d02cf0633baa312a8
Alex Kuznecov <alexander.kuznecov16@gmail.com>
2024-05-29 19:14:41 +0300

    comments and function destroy

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from tkinter import *
 import random
 
+user_clicked_btns = [] # user array with clicked buttons
+computer_clicked_btns = [] # computer array with clicked buttons
+
 # function for game
-def game(container):
+def game(container, root = None):
   # Clean full container
   for widget in container.winfo_children():
     widget.grid_forget()
     
-  user_clicked_btns = [] # user array with clicked buttons
-  computer_clicked_btns = [] # computer array with clicked buttons
+  global user_clicked_btns # user array with clicked buttons
+  global computer_clicked_btns # computer array with clicked buttons
   
   # function for winner checking
   def check_winner():
@@ -28,14 +31,15 @@ def game(container):
     for combination in winning_combinations:
       # if user clicked buttons by winning some combination
       if all(btn in user_clicked_btns for btn in combination):
-        result(container, winner='User') # send data to result function
+        result(container, winner='User', root=root) # send data to result function
         return True # return result
       
       # if computer clicked buttons by winning some combination
       elif all(btn in computer_clicked_btns for btn in combination):
-        result(container, winner='Computer') # send data to result function
+        result(container, winner='Computer', root=root) # send data to result function
         return True # return result
     return False # return False, because clicked buttons are not in combinations array
+
 
   # function where computer do click
   def computer_step():
@@ -51,7 +55,8 @@ def game(container):
       if check_winner():
         return
     else:
-      result(container, winner=None) # draw
+      result(container, winner=None, root = root) # draw
+
 
   # function for user clicked button
   def clicked_btn(btn):
@@ -177,32 +182,52 @@ def game(container):
   btn_8.grid(column=1, row=2)
   btn_9.grid(column=2, row=2)
   
+  
 # function for result processing
-def result(container, winner):
+def result(container, winner, root):
   # Clean full container
   for widget in container.winfo_children():
       widget.grid_forget()
-  
-  text = Label(container, text='Game result:')
-  text.grid(column=1, row=0)
   
   # check who is winner
   if winner == 'User':
     # user is winner
     text = Label(container, text='You won against computer!')
-    text.grid(column=1, row=0)
+    text.grid(column=2, row=0)
     
   elif winner == 'Computer':
     # computer is winner
     text = Label(container, text='You lost against computer!')
-    text.grid(column=1, row=0)
+    text.grid(column=2, row=0)
     
   else:
     # draw
     text = Label(container, text='You draw against computer!')
-    text.grid(column=1, row=0)
+    text.grid(column=2, row=0)
+    
+  # button for game restart
+  btn_restart = Button(container, text='Restart', command=lambda: restart_game(container), font=40) # clicking to the button will be called the function for game restart
+  btn_restart.grid(row=3, column=1, padx=20, pady=20) # Button position
+  btn_close = Button(container, text='Close', command=lambda: close_game(root), font=40) # button to close the app
+  btn_close.grid(row=3, column=3, padx=20, pady=20) # close button position
   
 
+# Function for values and game restart
+def restart_game(container):
+  global user_clicked_btns # user array with clicked buttons
+  global computer_clicked_btns # computer array with clicked buttons
+  
+  user_clicked_btns = [] # refresh user clicked buttons
+  computer_clicked_btns = [] # refresh computer clicked buttons
+  
+  game(container) # play new game
+
+
+# close app function
+def close_game(root):
+  root.destroy()
+  
+  
 # main function
 def main():
   root = Tk() # Creating an app window
@@ -213,8 +238,8 @@ def main():
   container.pack(expand=True) # Pack this container to root
   
   # buttons
-  btn_start = Button(container, text='Start', command=lambda: game(container), font=40) # button to start game
-  btn_close = Button(container, text='Close', command=root.destroy, font=40) # button to close the app
+  btn_start = Button(container, text='Start', command=lambda: game(container, root=root), font=40) # button to start game
+  btn_close = Button(container, text='Close', command=lambda: close_game(root), font=40) # button to close the app
   
   #buttons position
   btn_start.grid(row=0, column=0, padx=20, pady=20) # start button position
